71c68fbcec254827e0de6f60839b72896f13cb94
Bruno <brunoiannirodrigues1@gmail.com>
2025-02-19 20:45:30 -0300

    Commit 35
    Resolvendo exercício sobre List - Básico

---
+++ collections-java-api/src/list/OperacoesBasicas/Item.java
@@ -0,0 +1,35 @@
+package list.OperacoesBasicas;
+
+public class Item {
+
+    private String nome;
+    private double preco;
+    private int quatidade;
+
+    public Item(String nome, double preco, int quatidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quatidade = quatidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuatidade() {
+        return quatidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quatidade=" + quatidade +
+                '}';
+    }
+}

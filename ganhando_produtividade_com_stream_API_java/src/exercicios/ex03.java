75753075e2b9d91dfc49272cd612afe13e9d9e58
Bruno <brunoiannirodrigues1@gmail.com>
2025-02-25 21:05:01 -0300

    ex04
    Números pares

--- ganhando_produtividade_com_stream_API_java/src/exercicios/ex03.java
+++ ganhando_produtividade_com_stream_API_java/src/exercicios/ex03.java
@@ -8,10 +8,11 @@ public class ex03 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        boolean numerosPositivos = numeros.stream().allMatch( n -> n > 0);
+        boolean numerosPositivos = numeros.stream().allMatch(n -> n > 0);
 
         System.out.println(numerosPositivos);
     }
 
 
+    // allMatch() - Verifica se todos os números da lista são maiores que 0.
 }

226ea2e0d4f47db27e82bf4dff32d410dce3a42c
Bruno <brunoiannirodrigues1@gmail.com>
2025-03-12 19:33:51 -0300

    Facade

--- lab-padroes-projeto-java/src/one/digitalinovation/gof/Test.java
+++ lab-padroes-projeto-java/src/one/digitalinovation/gof/Test.java
@@ -1,5 +1,6 @@
 package one.digitalinovation.gof;
 
+import one.digitalinovation.gof.facade.Facade;
 import one.digitalinovation.gof.singleton.SingletonEager;
 import one.digitalinovation.gof.singleton.SingletonLazy;
 import one.digitalinovation.gof.singleton.SingletonLazyHolder;
@@ -28,6 +29,7 @@ public class Test {
 
         // Strategy
 
+        /**
         Comportamento Defensivo = new ComportamentoDefensivo();
         Comportamento normal = new ComportamentoNormal();
         Comportamento Agressivo = new ComportamentoAgressivo();
@@ -42,7 +44,12 @@ public class Test {
         robo.mover();
         robo.mover();
         robo.mover();
+         **/
 
+        // Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "1233445333");
 
     }
 }
